5a4e242f459e440a320547e37b65c712b95da6b1
Flavio Ipiranga <flavio.araujo@toradex.com>
2025-10-06 14:39:34 -0300

    Initial commit

---
+++ data/gfx_prebuilt/imagination/local/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/usr/local/lib"
+XML2_LIBS="-lxml2 -L/data/bthomasa/gfx_linux/checkin_2/img_driver/rogue/binary_vs640_linux_wayland_release/target_armhf/usr/local/lib -lz     -lm "
+XML2_INCLUDEDIR="-I/usr/local/include/libxml2"
+MODULE_VERSION="xml2-2.9.13"
+
